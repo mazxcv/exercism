afbcc8239956cd4a2d42e660b6a4658077017bf1
Maxim <mazxcc@gmail.com>
2024-09-03 22:22:16 +0300

    2. Generate wand energy.

--- animal-magic/animal_magic.go
+++ animal-magic/animal_magic.go
@@ -4,12 +4,12 @@ import "math/rand"
 
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	return rand.Intn(20)
+	return rand.Intn(21)
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
