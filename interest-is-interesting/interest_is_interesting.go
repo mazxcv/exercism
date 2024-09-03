9c535b637e51ac58c4a9cdcfb17df2782a7f2a36
Maxim <mazxcc@gmail.com>
2024-09-03 20:28:54 +0300

    1. Calculate the interest rate

---
+++ interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,29 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	if balance < 0 {
+		return float32(3.213)
+	} else if balance < 1000 {
+		return float32(0.5)
+	} else if balance < 5000 {
+		return float32(1.621)
+	} else {
+		return float32(2.475)
+	}
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	panic("Please implement the Interest function")
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	panic("Please implement the AnnualBalanceUpdate function")
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	panic("Please implement the YearsBeforeDesiredBalance function")
+}
