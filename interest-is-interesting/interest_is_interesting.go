d4c311b37e6539e7dd9a5eb0902302b1a06b5c05
Maxim <mazxcc@gmail.com>
2024-09-03 20:41:06 +0300

    optimize

--- interest-is-interesting/interest_is_interesting.go
+++ interest-is-interesting/interest_is_interesting.go
@@ -3,13 +3,13 @@ package interest
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
 	if balance < 0 {
-		return float32(3.213)
+		return 3.213
 	} else if balance < 1000 {
-		return float32(0.5)
+		return 0.5
 	} else if balance < 5000 {
-		return float32(1.621)
+		return 1.621
 	} else {
-		return float32(2.475)
+		return 2.475
 	}
 }
 
@@ -25,9 +25,10 @@ func AnnualBalanceUpdate(balance float64) float64 {
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	count := 0
-	for count = 0; balance < targetBalance; count++ {
+	years := 0
+	for balance < targetBalance {
 		balance = AnnualBalanceUpdate(balance)
+		years++
 	}
-	return count
+	return years
 }
