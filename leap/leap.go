95935b6ec0b03e6283a1648d926c754039f81584
Maxim <mazxcc@gmail.com>
2024-09-14 16:07:37 +0300

    leap

--- leap/leap.go
+++ leap/leap.go
@@ -1,7 +1,7 @@
 // Package leap is a library to determine leap year (in the Gregorian calendar)
 package leap
 
-// IsLeapYear determine and returns is leap year
+// IsLeapYear determine and returns if the passed in year is a leap year.
 // In every year that is evenly divisible by 4.
 // Unless the year is evenly divisible by 100,
 // in which case it's only a leap year if the year is also evenly divisible by 400.
