b6c6536c0c07b84ab85a3ee95c743478b8c08cc3
Maxim <mazxcc@gmail.com>
2024-09-14 20:42:32 +0300

    isogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,22 +1,20 @@
-// Package isogram is a small library for determine isogram
+// Package isogram is a small library for analyzing if a phrase is an isogram.
 package isogram
 
 import (
 	"strings"
 )
 
-// IsIsogram determine and return if is isogram
+// IsIsogram accepts a word and returns if it is an isogram.
 func IsIsogram(word string) bool {
 	testIsogram := map[string]int{}
 	for _, v := range strings.Split(strings.ToLower(word), "") {
-		_, ok := testIsogram[v]
-		if !ok {
-			testIsogram[v]++
-		} else {
+		if _, ok := testIsogram[v]; ok {
 			if v != " " && v != "-" {
 				return false
 			}
 		}
+		testIsogram[v]++
 	}
 	return true
 }
