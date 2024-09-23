cdc1f6fa1700b9df650d9ae998aa5c959ce5d3e7
Maxim <mazxcc@gmail.com>
2024-09-23 19:12:07 +0300

    pangram

---
+++ pangram/pangram.go
@@ -0,0 +1,29 @@
+package pangram
+
+import (
+	"regexp"
+	"strings"
+)
+
+type Pangram map[string]int
+
+func makePangram() map[string]int {
+	return Pangram{}
+}
+
+func IsPangram(input string) bool {
+	isWord := regexp.MustCompile(`[a-zA-Z]`)
+	pangram := makePangram()
+	for _, v := range strings.Split(strings.ToLower(input), "") {
+		if isWord.MatchString(v) {
+			pangram[v] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(pangram))
+
+	for k := range pangram {
+		keys = append(keys, k)
+	}
+	return len(keys) == 26
+}
