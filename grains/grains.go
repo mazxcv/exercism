c6a82c3f8c65ba201b9cfec8db3b72d243a40078
Maxim <mazxcc@gmail.com>
2024-09-23 21:35:19 +0300

    grains

--- grains/grains.go
+++ grains/grains.go
@@ -2,14 +2,13 @@ package grains
 
 import (
 	"fmt"
-	"math"
 )
 
 func Square(number int) (uint64, error) {
 	if number > 64 || number <= 0 {
 		return 0, fmt.Errorf("64 squares on a chessboard (")
 	}
-	return uint64(math.Pow(float64(2), float64(number-1))), nil
+	return 1 << uint(number-1), nil
 }
 
 func Total() uint64 {
