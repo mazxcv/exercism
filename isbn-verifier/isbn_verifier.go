e0383b5ba84a8ab5bfc8aeee32dad65e0988133f
Maxim <mazxcc@gmail.com>
2024-09-24 18:28:38 +0300

    isbn

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -9,16 +9,13 @@ func IsValidISBN(isbn string) bool {
 	acc := 0
 	m := 10
 
-	for _, v := range strings.Split(isbn, "") {
+	for _, v := range strings.Split(strings.Trim(isbn, "-"), "") {
 		x, err := strconv.Atoi(v)
 		if err != nil {
-			switch v {
-			case "-":
-				continue
-			case "X":
-				if m > 1 {
-					return false
-				}
+			switch {
+			case v == "X" && m > 1:
+				return false
+			case v == "X":
 				x = 10
 			default:
 				return false
