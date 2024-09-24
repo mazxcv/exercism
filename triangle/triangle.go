495e16ef5a7e671645f8635603f618c2671464b7
Maxim <mazxcc@gmail.com>
2024-09-24 22:12:40 +0300

    triangle

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,22 +1,17 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// package triangle is small library for determine type triangle
 package triangle
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
+// type Kind - type of triangle
 type Kind int
 
 const (
-	// Pick values for the following identifiers used by the test program.
 	NaT Kind = iota // not a triangle
 	Equ             // equilateral
 	Iso             // isosceles
 	Sca             // scalene
 )
 
-// KindFromSides should have a comment documenting it.
+// func KindFromSides takes the value of the three sides of a triangle and returns the type of the triangle
 func KindFromSides(a, b, c float64) Kind {
 	switch {
 	case a+b <= c || a+c <= b || b+c <= a:
