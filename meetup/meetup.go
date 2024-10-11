ebd9df55ebb76f40afbe444a123340f93a9b9419
Maxim <mazxcc@gmail.com>
2024-10-11 15:20:21 +0300

    meetup

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -14,5 +14,34 @@ const (
 )
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	firstDay := time.Date(year, month, 1, 0, 0, 0, 0, time.Local)
+	LastDay := time.Date(year, month+1, 0, 0, 0, 0, 0, time.Local).Day()
+	diffWeek := wDay - firstDay.Weekday()
+	if diffWeek < 0 {
+		diffWeek += 7
+	}
+	switch wSched {
+	case First:
+		return int(diffWeek) + 1
+	case Second:
+		return int(diffWeek) + 8
+	case Third:
+		return int(diffWeek) + 15
+	case Fourth:
+		return int(diffWeek) + 22
+	case Last:
+		if LastDay >= int(diffWeek)+29 {
+			return int(diffWeek) + 29
+		}
+		return int(diffWeek) + 22
+	case Teenth:
+		TeenthDay := time.Date(year, month, 13, 0, 0, 0, 0, time.Local).Weekday()
+		diffDay := wDay - TeenthDay
+		if diffDay < 0 {
+			diffDay += 7
+		}
+		return int(diffDay) + 13
+	default:
+		return 0
+	}
 }
