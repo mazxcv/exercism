6eb4167ddec9123448210f9186cb85c2925fce24
Maxim <mazxcc@gmail.com>
2024-10-11 14:53:57 +0300

    meetup create type WeekSchedule

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -2,7 +2,16 @@ package meetup
 
 import "time"
 
-// Define the WeekSchedule type here.
+type WeekSchedule int
+
+const (
+	First WeekSchedule = iota + 1
+	Second
+	Third
+	Fourth
+	Last
+	Teenth
+)
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
 	panic("Please implement the Day function")
